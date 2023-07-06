f13a2440bf9ab15cf639b22aab9f127e326e9070
LigerGX <jtadub@gmail.com>
2023-07-06 15:45:20 +1200

    5c.14

--- src/App.js
+++ src/App.js
@@ -35,6 +35,34 @@ const App = () => {
     window.localStorage.removeItem('user')
   }
 
+  const updateBlog = async (blog) => {
+    const updatedBlog = {
+      author: blog.author,
+      title: blog.title,
+      url: blog.url,
+      likes: blog.likes + 1,
+    }
+
+    try {
+      const res = await blogService.update(updatedBlog, blog.id)
+      const filteredBlogs = blogs.filter(item => item.id !== blog.id)
+      setBlogs([...filteredBlogs, { ...res, user }])
+    } catch (error) {
+      console.error(error.response.data)
+    }
+  }
+
+  const removeBlog = async (blog) => {
+    try {
+      if (window.confirm(`Remove ${blog.title} by ${blog.author}?`)) {
+        await blogService.remove(blog.id)
+        setBlogs(blogs.filter(item => item.id !== blog.id))
+      }
+    } catch (error) {
+      console.error.apply(error.response.data)
+    }
+  }
+
   const showNotification = (message, error) => {
     setNotification({ message, error })
     setTimeout(() => {
@@ -61,7 +89,12 @@ const App = () => {
       </Toggleable>
 
       {blogs.sort((a,b) => b.likes - a.likes).map(blog =>
-        <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} user={user} />
+        <Blog
+          key={blog.id}
+          blog={blog}
+          removeBlog={removeBlog}
+          updateBlog={updateBlog}
+        />
       )}
     </div>
   )
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,39 +1,10 @@
 import { useState } from 'react'
-import blogService from '../services/blogs'
 
-const Blog = ({ blog, blogs, setBlogs, user }) => {
+const Blog = ({ blog, removeBlog, updateBlog }) => {
   const [visible, setVisible] = useState(false)
 
   const toggleVisibility = () => {
     setVisible(!visible)
-  }
-
-  const handleUpdate = async () => {
-    const updatedBlog = {
-      author: blog.author,
-      title: blog.title,
-      url: blog.url,
-      likes: blog.likes + 1,
-    }
-
-    try {
-      const res = await blogService.update(updatedBlog, blog.id)
-      const filteredBlogs = blogs.filter(item => item.id !== blog.id)
-      setBlogs([...filteredBlogs, { ...res, user }])
-    } catch (error) {
-      console.error(error.response.data)
-    }
-  }
-
-  const handleRemove = async () => {
-    try {
-      if (window.confirm(`Remove ${blog.title} by ${blog.author}?`)) {
-        await blogService.remove(blog.id)
-        setBlogs(blogs.filter(item => item.id !== blog.id))
-      }
-    } catch (error) {
-      console.error.apply(error.response.data)
-    }
   }
 
   if (!visible) {
@@ -48,9 +19,9 @@ const Blog = ({ blog, blogs, setBlogs, user }) => {
     <div className="blogContainer">
       <p>{blog.title} by {blog.author} <span><button onClick={toggleVisibility}>Hide</button></span></p>
       <p>url: {blog.url}</p>
-      <p>Likes: {blog.likes} <span><button onClick={handleUpdate}>Like</button></span></p>
+      <p>Likes: {blog.likes} <span><button onClick={() => updateBlog(blog)}>Like</button></span></p>
       <p>Submitted by {blog.user.username}</p>
-      <button onClick={handleRemove}>Remove</button>
+      <button onClick={() => removeBlog(blog)}>Remove</button>
     </div>
   )
 }
